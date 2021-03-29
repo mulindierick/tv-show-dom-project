996a03de2fbe49dff88a90fa6a46977e4f43eba4
mulindierick <37551785+mulindierick@users.noreply.github.com>
2021-03-30 01:50:24 +0300

    tv shows level 100
    level 100 finished

--- script.js
+++ script.js
@@ -5,8 +5,34 @@ function setup() {
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  let rootElem = document.getElementById("root");
+  // loop through all the episodes 
+  episodeList.forEach(element => { 
+    let episodeGrouping = document.createElement("div")
+    let header = document.createElement("h3");
+    let mediumImage = document.createElement("img")
+    mediumImage.setAttribute("src", `${element.image.medium}`);
+    let summaryText = document.createElement("p")
+    let removeHtml = element.summary;
+    let htmlRemoved = removeHtml.replace(/<\/?[^>]+(>|$)/g, "");
+    let episodeName = document.createTextNode(`${element.name}`);
+    let SeasonNumber = document.createTextNode(` - S0${element.season}`);
+    let episodeNumber = document.createTextNode(`E0${element.number}`);
+    let episodeSummaryText = document.createTextNode(`${htmlRemoved}`);
+
+
+    header.appendChild(episodeName)
+    header.appendChild(SeasonNumber)
+    header.appendChild(episodeNumber)
+    summaryText.appendChild(episodeSummaryText)
+
+    episodeGrouping.appendChild(header)
+    episodeGrouping.appendChild(mediumImage)
+    episodeGrouping.appendChild(summaryText)
+    
+
+    rootElem.appendChild(episodeGrouping)
+  });
 }
 
 window.onload = setup;
